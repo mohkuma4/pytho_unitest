820ddbb8e91902a60c7168c325b2e13d3cd17d23
mohkuma4 <mohkuma4@cisco.com>
2019-08-02 11:28:21 +0530

    test commit

---
+++ test_calc.py
@@ -0,0 +1,17 @@
+import unittest
+import calc
+
+
+class TestCalc(unittest.TestCase):
+
+    def test_add(self):
+        self.assertEqual(calc.add(10, 5), 15)
+
+
+    def test_multiply(self):
+        self.assertEqual(calc.multiply(10, 5), 15)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
