20f520e9eb9f095f59571636460ca23c5d9035dd
Mohan Kumar <mohkuma4@cisco.com>
2019-08-02 17:57:32 +0530

    trigger remote buld

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,4 @@
 FROM python:3
 ADD test_calc.py /
+ADD calc.py /
 CMD ["python", "./test_calc.py"]
--- test_calc.py
+++ test_calc.py
@@ -9,7 +9,7 @@ class TestCalc(unittest.TestCase):
 
 
     def test_multiply(self):
-        self.assertEqual(calc.multiply(10, 5), 15)
+        self.assertEqual(calc.multiply(10, 5), 50)
 
 
 
